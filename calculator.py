33d5ce7fe62d7498bd1774a7a8d72f047ed01f42
jhamilex <jhamilex.jgr@gmail.com>
2020-06-24 22:44:28 -0400

    implementando factorial

--- calculator.py
+++ calculator.py
@@ -1,47 +1,7 @@
 class Calculator:
-    pass
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    def factorial(self, x):
+        f = 1
+        for i in range(1, x+1):
+            f = f * i
+        self.value = f
--- test_fibo.py
+++ test_fibo.py
@@ -1,23 +1,24 @@
-# Cargamos el módulo unittest
+
 import unittest  
 
-# Importamos la clase calculadora
+
 from calculator import Calculator
 
-# Creamos una clase heredando de TestCase
+
 class TestMyCalculator(unittest.TestCase):  
 
     def setUp(self):
         self.calc = Calculator()
-    
-    # Creamos una prueba para probar un valor inicial
+
+
     def test_calc1(self):
         self.calc.factorial(1)
         self.assertEqual(1, self.calc.value)
-        
+
+
     def test_calc2(self):
         self.calc.factorial(0)
-        self.assertEqual(2, self.calc.value)
+        self.assertEqual(1, self.calc.value)
 
 
 if __name__ == '__main__':
