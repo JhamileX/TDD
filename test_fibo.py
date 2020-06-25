e821d69afd5c89a81696030c31ef8a8c1521bfe9
jhamilex <jhamilex.jgr@gmail.com>
2020-06-24 22:53:24 -0400

    refactorizar

--- test_fibo.py
+++ test_fibo.py
@@ -14,11 +14,13 @@ class TestMyCalculator(unittest.TestCase):
     def test_calc1(self):
         self.calc.factorial(1)
         self.assertEqual(1, self.calc.value)
-
-
-    def test_calc2(self):
         self.calc.factorial(0)
         self.assertEqual(1, self.calc.value)
+
+
+    # def test_calc2(self):
+    #     self.calc.factorial(0)
+    #     self.assertEqual(1, self.calc.value)
 
 
 if __name__ == '__main__':
